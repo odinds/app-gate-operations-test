0816d1ca74bba2591cbf401bb7108d7d5fe37bd4
Daniel Sarmiento <odinds@gmail.com>
2021-05-07 14:19:08 -0500

    project ini spring boot

---
+++ operarions/src/main/java/com/appegate/task/operarions/OperarionsApplication.java
@@ -0,0 +1,13 @@
+package com.appegate.task.operarions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperarionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperarionsApplication.class, args);
+	}
+
+}
---
+++ operarions/src/test/java/com/appegate/task/operarions/OperarionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appegate.task.operarions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperarionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
